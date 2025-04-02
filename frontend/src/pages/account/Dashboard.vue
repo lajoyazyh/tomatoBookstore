26c3f5064d88341851120d90cab1aca5a2e01da9
Simahg <smhg1258@163.com>
2025-04-02 14:31:22 +0800

    dashboard avatarChange func

--- frontend/src/pages/account/Dashboard.vue
+++ frontend/src/pages/account/Dashboard.vue
@@ -3,6 +3,7 @@ import {ElForm, ElFormItem, ElMessage} from "element-plus"
 import { ref, computed, onMounted } from 'vue'
 import {router} from '../../router'
 import { userInfo, userInfoUpdate } from '../../api/accounts'
+import { uploadImage } from "../../api/images.ts";
 
 // 更改用户信息时，不必也不能更改username，username只作为标志
 const username = sessionStorage.getItem('username') || ''
@@ -39,6 +40,23 @@ onMounted(async () => {
 
   // 如果需要钩子获取其他信息在这里补充
 })
+
+function handleFileChange(file: any) {
+  const formData = new FormData();
+  formData.append('file', file.raw);
+
+  uploadImage(formData).then(res => {
+    if (res.data.code === '000') {
+      avatar.value = res.data.result; // 存储上传的图片 URL
+      currentFile.value = file; // 存储当前文件
+      ElMessage.success('文件上传成功！');
+    } else {
+      ElMessage.error(res.data.msg || '文件上传失败，请稍后再试！');
+    }
+  }).catch(error => {
+    ElMessage.error('文件上传失败：' + error.message);
+  });
+}
 
 function handleUpdate() {
   userInfoUpdate({
