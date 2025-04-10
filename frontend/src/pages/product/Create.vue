f7b95bcc1b151d90b7a47789fef4355bf5fb628f
Simahg <smhg1258@163.com>
2025-04-10 20:47:11 +0800

    product create page

--- frontend/src/pages/product/Create.vue
+++ frontend/src/pages/product/Create.vue
@@ -17,7 +17,7 @@ const description = ref('')
 const currentFile = ref(null) // cover file
 const cover = ref('')
 const detail = ref('')
-const specifications = ref([])
+const specifications = ref<Specification[]>([]);
 
 const newSpecification = ref<{
   item: string,
@@ -58,10 +58,16 @@ function addNewSpecification() {
   // 不允许创建空的规格描述
   if (addSpecificationDisabled.value) return;
 
-  // 用一个深拷贝更新specifications，避免直接修改
-  const specificationsCopy = JSON.parse(JSON.stringify(specifications));
-  specificationsCopy.push(newSpecification.value);
-  specifications.value = specificationsCopy;
+  // 创建新对象而非拷贝赋值，以确保响应
+  specifications.value = [
+    ...specifications.value,
+    {
+      id: -1,
+      item: newSpecification.value.item,
+      value: newSpecification.value.value,
+      productId: -1,
+    },
+  ];
 
   newSpecification.value.item = '';
   newSpecification.value.value = '';
@@ -95,9 +101,163 @@ function handleCreateProduct() {
 </script>
 
 <template>
+  <div class="product-create-container">
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>创建商品</span>
+        </div>
+      </template>
 
+      <el-form label-width="120px" :model="{}">
+        <!-- 商品标题 -->
+        <el-form-item label="商品标题" :required="true">
+          <el-input
+              v-model="title"
+              placeholder="请输入商品标题"
+              clearable
+              style="width: 100%"
+          ></el-input>
+        </el-form-item>
+
+        <!-- 商品价格 -->
+        <el-form-item label="商品价格" :required="true">
+          <el-input-number
+              v-model="price"
+              :min="0"
+              placeholder="请输入商品价格"
+              style="width: 100%"
+          ></el-input-number>
+        </el-form-item>
+
+        <!-- 商品评分 -->
+        <el-form-item label="商品评分" :required="true">
+          <el-input-number
+              v-model="rate"
+              :min="0"
+              :max="10"
+              placeholder="请输入商品评分"
+              style="width: 100%"
+          ></el-input-number>
+        </el-form-item>
+
+        <!-- 商品描述 -->
+        <el-form-item label="商品描述">
+          <el-input
+              v-model="description"
+              type="textarea"
+              placeholder="请输入商品描述"
+              :rows="3"
+              style="width: 100%"
+          ></el-input>
+        </el-form-item>
+
+        <!-- 商品详情 -->
+        <el-form-item label="商品详情">
+          <el-input
+              v-model="detail"
+              type="textarea"
+              placeholder="请输入商品详情"
+              :rows="5"
+              style="width: 100%"
+          ></el-input>
+        </el-form-item>
+
+        <!-- 商品封面 -->
+        <el-form-item label="商品封面">
+          <el-upload
+              class="upload-demo"
+              :auto-upload="false"
+              :show-file-list="false"
+              :on-change="handleFileChange"
+              accept="image/*"
+          >
+            <el-button type="primary">点击上传</el-button>
+            <template #tip>
+              <div class="el-upload__tip">只能上传图片文件</div>
+            </template>
+          </el-upload>
+          <div v-if="cover" class="cover-preview">
+            <el-image :src="cover" fit="cover" style="width: 100px; height: 100px"></el-image>
+          </div>
+        </el-form-item>
+
+        <!-- 商品规格 -->
+        <el-form-item label="商品规格">
+          <div class="specification-item" v-for="(spec, index) in specifications" :key="index">
+            <el-input
+                v-model="spec.item"
+                placeholder="规格名称"
+                style="width: 150px; margin-right: 10px"
+                :disabled="true"
+            ></el-input>
+            <el-input
+                v-model="spec.value"
+                placeholder="规格值"
+                style="width: 150px; margin-right: 10px"
+                :disabled="true"
+            ></el-input>
+            <el-button type="danger" size="small" @click="specifications.splice(index, 1)">
+              删除
+            </el-button>
+          </div>
+
+          <div style="margin-top: 10px">
+            <el-input
+                v-model="newSpecification.item"
+                placeholder="规格名称"
+                style="width: 150px; margin-right: 10px"
+            ></el-input>
+            <el-input
+                v-model="newSpecification.value"
+                placeholder="规格值"
+                style="width: 150px; margin-right: 10px"
+            ></el-input>
+            <el-button
+                type="primary"
+                :disabled="addSpecificationDisabled"
+                @click="addNewSpecification"
+            >
+              添加规格
+            </el-button>
+          </div>
+        </el-form-item>
+
+        <!-- 提交按钮 -->
+        <el-form-item>
+          <el-button
+              type="primary"
+              :disabled="createDisabled"
+              @click="handleCreateProduct"
+              style="width: 100%"
+          >
+            创建商品
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <style scoped>
+.product-create-container {
+  max-width: 800px;
+  margin: 20px auto;
+}
 
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.specification-item {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.cover-preview {
+  margin-top: 10px;
+}
 </style>
