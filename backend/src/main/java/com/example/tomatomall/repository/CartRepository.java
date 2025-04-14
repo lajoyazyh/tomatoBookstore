06b820941ee984eec458e8d7f33a458c03ccdad4
lajoyazyh <231880098@smail.nju.edu.cn>
2025-04-14 21:33:24 +0800

    fix the FindByUseridandProductid problem

--- backend/src/main/java/com/example/tomatomall/repository/CartRepository.java
+++ backend/src/main/java/com/example/tomatomall/repository/CartRepository.java
@@ -2,11 +2,13 @@ package com.example.tomatomall.repository;
 
 import com.example.tomatomall.po.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 
+    @Query("SELECT c FROM Cart c WHERE c.account.id = ?1 AND c.product.id = ?2")
     Cart findByUserIdAndProductId(Integer userId, Integer productId);
 }
