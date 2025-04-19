654b2c8b5ae692c47c3d361fe9f73484522f63dd
lajoyazyh <231880098@smail.nju.edu.cn>
2025-04-19 21:12:20 +0800

    hotfix adding comment

--- backend/src/main/java/com/example/tomatomall/po/Cart.java
+++ backend/src/main/java/com/example/tomatomall/po/Cart.java
@@ -18,19 +18,19 @@ public class Cart {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    @Column(name = "cartItemId")
+    @Column(name = "cartItemId", columnDefinition = "COMMENT '购物车商品ID'")
     private Integer cartItemId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userId", referencedColumnName = "id", nullable = false)
+    @JoinColumn(name = "userId", referencedColumnName = "id", nullable = false, columnDefinition = "COMMENT '用户id，关联用户表'")
     private Account account; // 关联用户表
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "productId", referencedColumnName = "id", nullable = false)
+    @JoinColumn(name = "productId", referencedColumnName = "id", nullable = false, columnDefinition = "COMMENT '商品id，关联商品表'")
     private Product product; // 关联商品表
 
     @Basic
-    @Column(name = "quantity", nullable = false)
+    @Column(name = "quantity", nullable = false, columnDefinition = "COMMENT '商品数量，默认为1'")
     private Integer quantity = 1; // 商品数量，默认为1
 
     public CartVO toVO() {
