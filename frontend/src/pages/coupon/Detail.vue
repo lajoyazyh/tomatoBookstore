011e6361515fee48e7eef3ad6c7e9e63224bf96c
Simahg <smhg1258@163.com>
2025-05-22 19:42:42 +0800

    coupon scripts finished

--- frontend/src/pages/coupon/Detail.vue
+++ frontend/src/pages/coupon/Detail.vue
@@ -1,4 +1,114 @@
 <script setup lang="ts">
+import {computed, onMounted, ref} from 'vue'
+import {useRoute} from 'vue-router';
+import {ElMessage} from "element-plus";
+import {router} from '../../router';
+import { getTypeText, getStatusText, formatDateToString, formatStringToDate } from "../../api/coupon.ts";
+import { updateCoupon, getTheCoupon } from "../../api/coupon.ts";
+import type { UpdateCouponInfo } from "../../api/coupon.ts";
+
+// 修改优惠券信息需要STAFF权限
+const role = sessionStorage.getItem('role');
+if (role != 'STAFF') router.push('/');
+const isEditing = ref(false);
+
+const route = useRoute();
+const couponId = Number(route.params.couponId);
+
+const couponInfo = ref<{
+  couponName: string,
+  couponType: string,
+  discountAmount?: number     // when type is 'FIXED' or 'THRESHOLD'
+  discountPercentage?: number // when type is 'PERCENTAGE'
+  minPurchaseAmount?: number  // when type is 'THRESHOLD'
+  validFrom: Date,
+  validUntil: Date,
+  status?: string,
+  description?: string
+}>({
+  couponName: '优惠券',
+  couponType: '',
+  validFrom: new Date(),
+  validUntil: new Date(),
+})
+
+onMounted(async () => {
+  try {
+    getTheCoupon(couponId).then((res) => {
+      if (res.data.code == '200') {
+        couponInfo.value.couponName = res.data.data.couponName
+        couponInfo.value.couponType = res.data.data.couponType
+        couponInfo.value.discountAmount = res.data.data.discountAmount
+        couponInfo.value.discountPercentage = res.data.data.discountPercentage
+        couponInfo.value.minPurchaseAmount = res.data.data.minPurchaseAmount
+        couponInfo.value.validFrom = formatStringToDate(res.data.data.validFrom)
+        couponInfo.value.validUntil = formatStringToDate(res.data.data.validUntil)
+        couponInfo.value.status = res.data.data.status
+        couponInfo.value.description = res.data.data.description
+      }
+      else {
+        ElMessage.error(res.data.msg)
+      }
+    })
+  } catch (error) {
+    ElMessage.error('获取信息失败！');
+    console.log(error);
+  }
+})
+
+const hasCouponName = computed(() => !!couponInfo.value.couponName)
+const hasCouponType = computed(() => !!couponInfo.value.couponType)
+const hasDiscountAmount = computed(() => !!couponInfo.value.discountAmount)
+const hasDiscountPercentage = computed(() => !!couponInfo.value.discountPercentage)
+const hasMinPurchaseAmount = computed(() => !!couponInfo.value.minPurchaseAmount)
+const hasValidFrom = computed(() => !!couponInfo.value.validFrom)
+const hasValidUntil = computed(() => !!couponInfo.value.validUntil)
+
+const hasNecessaryInfo =  computed(() => {
+  return hasCouponName.value && hasCouponType.value && hasValidFrom.value && hasValidUntil.value
+})
+const isValidTypeInfo = computed(() => {
+  const case1 = couponInfo.value.couponType === 'FIXED' && hasDiscountAmount.value;
+  const case2 = couponInfo.value.couponType === 'PERCENTAGE' && hasDiscountPercentage.value;
+  const case3 = couponInfo.value.couponType === 'THRESHOLD' && hasDiscountAmount.value && hasMinPurchaseAmount.value;
+  return case1 || case2 || case3
+})
+const isValidTimeInfo = computed(() => couponInfo.value.validFrom < couponInfo.value.validUntil)
+const UpdateCouponDisabled = computed(() => {
+  return !(role == 'STAFF' && hasNecessaryInfo.value && isValidTypeInfo.value && isValidTimeInfo.value)
+})
+
+function createUpdateCouponInfo(): UpdateCouponInfo {
+  return {
+    couponId: couponId,
+    couponName: couponInfo.value.couponName,
+    couponType: couponInfo.value.couponType,
+    discountAmount: couponInfo.value.discountAmount,
+    discountPercentage: couponInfo.value.discountPercentage,
+    minPurchaseAmount: couponInfo.value.minPurchaseAmount,
+    validFrom: formatDateToString(couponInfo.value.validFrom),
+    validUntil: formatDateToString(couponInfo.value.validUntil),
+    status: couponInfo.value.status,
+    description: couponInfo.value.description,
+  }
+}
+
+function handleUpdateCoupon(updateInfo: UpdateCouponInfo) {
+  try {
+    updateCoupon(createUpdateCouponInfo()).then(res => {
+      if (res.data.code === '200') {
+        ElMessage.success('修改成功！');
+        // 刷新
+        router.go(0);
+      } else {
+        ElMessage.error(res.data.msg)
+      }
+    })
+  } catch (error) {
+    ElMessage.error('修改失败！请联系技术部门');
+    console.log(error);
+  }
+}
 
 </script>
 
--- frontend/src/pages/coupon/Manage.vue
+++ frontend/src/pages/coupon/Manage.vue
@@ -1,4 +1,80 @@
 <script setup lang="ts">
+import {onMounted, ref} from 'vue'
+import {ElMessage} from "element-plus";
+import {router} from '../../router';
+import { getTypeText, getStatusText, formatStringToDate } from "../../api/coupon.ts";
+import { getAllCoupons, deleteTheCoupon, updateCouponStatus } from "../../api/coupon.ts";
+
+// 删除优惠券需要STAFF权限
+const role = sessionStorage.getItem('role');
+
+type coupon = {
+  couponId: number,
+  couponName: string,
+  couponType: string,
+  discountAmount?: number     // when type is 'FIXED' or 'THRESHOLD'
+  discountPercentage?: number // when type is 'PERCENTAGE'
+  minPurchaseAmount?: number  // when type is 'THRESHOLD'
+  validFrom: Date,          // example: 2024-01-01T00:00:00
+  validUntil: Date,            // example: 2024-01-31T23:59:59
+  status?: string,
+  description?: string
+}
+const coupons = ref<Array<coupon>>([]);
+
+onMounted(async () => {
+  try {
+    const response = await getAllCoupons();
+    if (response.data.code === '200') {
+      coupons.value = response.data.data.map((coupon: any) => ({
+        couponId: coupon.id,
+        couponName: coupon.couponName,
+        couponType: coupon.couponType,
+        discountAmount: coupon.discountAmount,
+        discountPercentage: coupon.discountPercentage,
+        minPurchaseAmount: coupon.minPurchaseAmount,
+        validFrom: formatStringToDate(coupon.validFrom),
+        validUntil: formatStringToDate(coupon.validUntil),
+        status: coupon.status,
+        description: coupon.description,
+      }));
+    }
+  } catch (error) {
+    ElMessage.error('获取信息失败！');
+    console.log(error);
+  }
+})
+
+function updateStatus(id: number, status: string) {
+  try {
+    updateCouponStatus(
+    {couponId: id, status: status}).then(res => {
+      if (res.data.code === '200') {
+        ElMessage.success('更新状态成功！');
+      } else {
+        ElMessage.error(res.data.msg)
+      }
+    })
+  } catch (error) {
+    ElMessage.error('更新状态失败！');
+    console.log(error);
+  }
+}
+
+function deleteCoupon(id: number) {
+  try {
+    deleteTheCoupon(id).then(res => {
+      if (res.data.code === '200') {
+        ElMessage.success('删除成功！');
+      } else {
+        ElMessage.error(res.data.msg)
+      }
+    })
+  } catch (error) {
+    ElMessage.error('删除失败！');
+    console.log(error);
+  }
+}
 
 </script>
 
